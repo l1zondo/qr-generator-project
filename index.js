22502af9484d811cd7e7170b5155be691ed0093f
l1zondo <lizondofrancoezequiel@gmail.com>
2023-02-28 17:14:16 -0300

    faltan solucionar errores

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const download = document.querySelector(".download");
 const dark = document.querySelector(".dark");
 const light = document.querySelector(".light");
-const qrContainer = document.querySelector("#qrCode");
+const qrContainer = document.querySelector("#qr-code");
 const qrText = document.querySelector(".qr-text");
 const shareBtn = document.querySelector(".share-btn");
 const sizes = document.querySelector(".sizes");
@@ -14,7 +14,7 @@ shareBtn.addEventListener("click", handleShare);
 
 const defaultUrl = "https://www.instagram.com/dolman.ar/";
 let colorLight = "#fff",
-    colorDark = "000",
+    colorDark = "#000",
     text = defaultUrl,
     size = 300;
 
@@ -40,7 +40,7 @@ function handleQRText(e){
 
 async function generateQRCode(){
     qrContainer.innerHTML = "";
-    new generateQRCode("qr-code", {
+    new QRCode("qr-code", {
         text,
         height: size,
         width: size,
@@ -54,6 +54,35 @@ async function handleShare(){
     setTimeout(async()=>{
         try{
             const base64url =await resolveDataUrl();
+            const blob = awati (await fetch(base64url)).blob();
+            const file = new File([blob], "QRCode.png", {
+                type: blob.type,
+            });
+            await navigator.share({
+                files: [file],
+                title: text,
+            });
+        }catch (error){
+            alert("Your browser doesn't support sharing");
         }
+    }, 100);
+}
+function handleSize(e){
+    size = e.target.value;
+    generateQRCode();
+}
+
+function resolveDataUrl(){
+    return new Promise ((resolve,reject) =>{
+        setTimeout(()=>{
+            const img = document.querySelector("#qr-code img");
+            if(img.currentSrc){
+                resolve(img.currentSrc);
+                return;
+            }
+            const canvas = document.querySelector("canvas");
+            resolve(canvas.toDataURL());
+        }, 50);
     })
 }
+generateQRCode();
