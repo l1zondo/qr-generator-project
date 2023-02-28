2318f9d5f49c1cbe660a4fcc56843db6bb28d335
l1zondo <lizondofrancoezequiel@gmail.com>
2023-02-28 16:35:02 -0300

    js qr added

--- index.js
+++ index.js
@@ -13,3 +13,47 @@ sizes.addEventListener("change", handleSize);
 shareBtn.addEventListener("click", handleShare);
 
 const defaultUrl = "https://www.instagram.com/dolman.ar/";
+let colorLight = "#fff",
+    colorDark = "000",
+    text = defaultUrl,
+    size = 300;
+
+function handleDarkColor(e){
+    colorDark = e.target.value,
+    generateQRCode();
+}
+
+function handleLightColor(e){
+    colorLight = e.target.value,
+    generateQRCode();
+}
+
+
+function handleQRText(e){
+    const value = e.target.value;
+    text = value;
+    if (!value){
+        text = defaultUrl;
+    }
+    generateQRCode()
+}
+
+async function generateQRCode(){
+    qrContainer.innerHTML = "";
+    new generateQRCode("qr-code", {
+        text,
+        height: size,
+        width: size,
+        colorLight,
+        colorDark,
+    });
+    download.href = await resolveDataUrl();
+}
+
+async function handleShare(){
+    setTimeout(async()=>{
+        try{
+            const base64url =await resolveDataUrl();
+        }
+    })
+}
